23e3c249e3e4692f8f9ac6781249ac89a98930e7
ahmar-husain <ahmar.husain@maersk.com>
2024-05-05 13:21:33 +0530

    Tree finished

--- src/nucleotidz.datastructure/BST/Tree.cs
+++ src/nucleotidz.datastructure/BST/Tree.cs
@@ -1,23 +1,74 @@
-﻿namespace nucleotidz.datastructure.BST
+﻿using System.Xml.Linq;
+
+namespace nucleotidz.datastructure.BST
 {
     public class Tree
     {
-        public Node Insert(Node node,int value)
+        public Node Insert(Node node, int value)
         {
             if (node == null)
             {
                 node = new Node();
                 node.value = value;
             }
-            else if( value< node.value)
+            else if (value < node.value)
             {
-                node.Left=Insert(node.Left ,value);
+                node.Left = Insert(node.Left, value);
             }
             else if (value > node.value)
             {
                 node.Right = Insert(node.Right, value);
             }
             return node;
+        }
+        public Node GetCommonParent(Node first, Node second, Node root)
+        {
+            if (first.value < root.value && second.value < root.value)
+            {
+                GetCommonParent(first, second, root.Left);
+
+            }
+            if (first.value > root.value && second.value > root.value)
+            {
+                GetCommonParent(first, second, root.Right);
+            }
+            return root;
+        }
+
+        public Node GetParent(Node node, Node root)
+        {
+            if (node.value < root.value)
+            {
+                if (node.value != root.Left?.value)
+                {
+                    GetParent(node, root.Left);
+                }
+
+            }
+            if (node.value > root.value)
+            {
+                if (node.value != root.Right?.value)
+                {
+                    GetParent(node, root.Right);
+                }
+            }
+            return root;
+        }
+        public int GetDistance(Node node, Node root, int i = 0)
+        {
+            if (node.value < root.value)
+            {
+                i++;
+                GetDistance(node, root.Left,i);
+
+            }
+            if (node.value > root.value)
+            {
+                i++;
+                GetDistance(node, root.Right,i);
+
+            }
+            return i;
         }
     }
 }
--- src/nucleotidz.datastructure/Program.cs
+++ src/nucleotidz.datastructure/Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using nucleotidz.datastructure.BST;
+using System.ComponentModel.Design.Serialization;
 
 Console.WriteLine("Hello, World!");
-CreateTree();
+FindDistance();
+
+
 Console.ReadLine();
 
-static void CreateTree()
+static Node CreateTree()
 {
-    int[] nodeValues= { 11, 20, 8, 2, 3, 4, 50, 1 };
+    int[] nodeValues = { 11, 20, 8, 2, 3, 4, 50, 1 };
     Node root = new();
     root.value = 10;
     //Create root Node
@@ -18,4 +21,25 @@ static void CreateTree()
     {
         new Tree().Insert(root, value);
     }
+
+    return root;
+}
+static void FindParent()
+{
+    var tree = CreateTree();
+    new Tree().GetParent(new Node { value = 2 }, tree);
+
+}
+static void FindCommonParent()
+{
+    var tree = CreateTree();
+    new Tree().GetCommonParent(new Node { value = 1 }, new Node {value= 4 }, tree);
+
+}
+
+static void FindDistance()
+{
+    var tree = CreateTree();
+    new Tree().GetDistance(new Node { value = 3 }, tree);
+
 }
